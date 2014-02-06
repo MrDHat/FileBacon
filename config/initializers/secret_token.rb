807bd7fc1360719247c3fcf060bc51d5845338d4
MrDHat <ktyaks@gmail.com>
2014-02-06 15:30:18 +0530

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FileBacon::Application.config.secret_key_base = '11454b9c9a730209721bf165b40b0b9d982a6a0bf29f0359088af99234931f2fcf05b187c98f87ff45d55279f0cea0e778a7e5f8f1bec21d095a4fdbb7af3e31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FileBacon::Application.config.session_store :cookie_store, key: '_FileBacon_session'
